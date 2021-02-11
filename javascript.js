4d39d2326e4e554af1ef481791d525ddb23ddbe4
Daniel de Souza <ddanielsouza200@gmail.com>
2021-02-10 22:54:56 -0300

    Transaction table rows animation

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+window.onload = () => {
+    animeScroll();
+};
+
 const Modal = {
     open() {
         document
@@ -9,4 +13,27 @@ const Modal = {
             .querySelector('.modal-overlay')
             .classList.remove('active');
     }
+}
+
+const target = document.querySelectorAll('.left-animation');
+const animationClass = 'animate-table';
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 6);
+    target.forEach(function(e){
+        if(windowTop > e.offsetTop) {
+            e.classList.add(animationClass);
+        } else {
+            e.classList.remove(animationClass);
+        }
+    });
+}
+
+if(target.length) {
+
+window.addEventListener('scroll', function() {
+    animeScroll();
+    console.log('kaskdasl')
+})
+
 }
